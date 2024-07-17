ab19e336186486b7d5b00a58e60b312d9327717a
Carnegie Hall <genw.growth@gmail.com>
2024-07-17 15:03:47 -0400

    com.md

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// const { validateUseBuiltInsOption } = require("@babel/preset-env/lib/normalize-options")
+
+// Write your solution in this fix.
+const employee = {
+    name: "Dre",
+    streetAddress: "9th Ave",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+    [key]: value,
+};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee [key] = value;
+        return employee;
+};
+function deleteFromEmployeeByKey(employee, key) {
+    const ralph = {...employee};
+    delete ralph[key]
+    return ralph
+}
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key]
+    return employee
+}
